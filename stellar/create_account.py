bbf2936d427acfc16ea38b03f464af3068fe0636
FreedomChase <freedom.chase@hotmail.com>
2021-12-02 04:44:03 +0100

    Delete create_account.py

--- stellar/create_account.py
+++
@@ -1,9 +0,0 @@
-from stellar_sdk.keypair import Keypair
-import requests
-# Configure StellarSdk to talk to the horizon instance hosted by Stellar.org
-horizon_url = "https://horizon.stellar.org"
-
-account = Keypair.random()
-secret = account.secret
-public_key = account.public_key
-
