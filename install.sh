9ba08280021f51c1f22903373cb8c20caa8a29d3
FreedomChase <freedom.chase@hotmail.com>
2021-12-03 13:37:42 +0000

    updated install.sh

--- install.sh
+++ install.sh
@@ -2,13 +2,13 @@
 
 cd stellar
 
-pip3 freeze > requirements.txt
-
 pip3 install django==3.2.9
 
 pip3 install -U stellar-sdk
 
 pip3 install qrcode[pil]
+
+pip3 freeze > requirements.txt
 
 python3 manage.py migrate
 
