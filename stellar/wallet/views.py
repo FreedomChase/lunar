47d4e3c81c4b1a533461a8be3292ce181ef4d5e5
FreedomChase <freedom.chase@hotmail.com>
2021-12-04 02:08:29 +0000

    stellar network login eroor handling fixed

--- stellar/wallet/views.py
+++ stellar/wallet/views.py
@@ -72,47 +72,46 @@ def activate(request):
     # Inactive Account:
     # It has to funded with the minimum balance of 5 XML before activation.
     # Generate QR code and deposit instructions
-    try:
-        account = requests.get(account_url + public_key, data={})
-        data = account.json()
-        try:
-            data['id']
-        except NameError:
-            return render(request, template, instruct_context)
-    except exceptions.NotFoundError:
-        return render(request, template, instruct_context)
 
-    # Active Account
+    account = requests.get(account_url + public_key, data={})
     data = account.json()
-    # Store some variables in the session
-    request.session['wallet_id'] = data['id']
-    request.session['transactions_url'] = string_cleaner(
-        data['_links']['transactions']['href'])
-    request.session['operations_url'] = string_cleaner(
-        data['_links']['operations']['href'])
-    request.session['payments_url'] = string_cleaner(
-        data['_links']['payments']['href'])
-    request.session['effects_url'] = string_cleaner(
-        data['_links']['effects']['href'])
-    request.session['offers_url'] = string_cleaner(
-        data['_links']['offers']['href'])
-    request.session['trades_url'] = string_cleaner(
-        data['_links']['trades']['href'])
-    request.session['data_url'] = string_cleaner(
-        data['_links']['data']['href'])
 
-    for asset in data['balances']:
-        if asset['asset_type'] == 'native':
-            asset['asset_code'] = 'XLM'
+    try:
+        data['id']
+    except KeyError:
+        return render(request, template, instruct_context)
+    else:
+        # Active Account
+        data = account.json()
+        # Store some variables in the session
+        request.session['wallet_id'] = data['id']
+        request.session['transactions_url'] = string_cleaner(
+            data['_links']['transactions']['href'])
+        request.session['operations_url'] = string_cleaner(
+            data['_links']['operations']['href'])
+        request.session['payments_url'] = string_cleaner(
+            data['_links']['payments']['href'])
+        request.session['effects_url'] = string_cleaner(
+            data['_links']['effects']['href'])
+        request.session['offers_url'] = string_cleaner(
+            data['_links']['offers']['href'])
+        request.session['trades_url'] = string_cleaner(
+            data['_links']['trades']['href'])
+        request.session['data_url'] = string_cleaner(
+            data['_links']['data']['href'])
 
-    data_context = {
-        'id': data['id'],
-        'qr': '.' + QRurl,
-        'last_modified': data['last_modified_time'],
-        'balances': data['balances']
-    }
-    request.session['session_data'] = data_context
-    return redirect('/dashboard/')
+        for asset in data['balances']:
+            if asset['asset_type'] == 'native':
+                asset['asset_code'] = 'XLM'
+
+        data_context = {
+            'id': data['id'],
+            'qr': '.' + QRurl,
+            'last_modified': data['last_modified_time'],
+            'balances': data['balances']
+        }
+        request.session['session_data'] = data_context
+        return redirect('/dashboard/')
 
 
 def qr_from_key(key, location):
